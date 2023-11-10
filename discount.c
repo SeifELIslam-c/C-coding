28c798052da3d042d54666130d210d062e92d812
Seif El Islam <145872339+SeifELIslam-c@users.noreply.github.com>
2023-11-10 19:12:26 +0100

    discount.c

--- discount.c
+++ discount.c
@@ -2,11 +2,9 @@
 #include <cs50.h>
 #include <string.h>
 
-double FreeInputDiscount(void){
-    double percent = get_double("Percentage will be from 5 to 100; please enter your percentage: ");
+void FreeInputDiscount(double percent){
     if(percent<5 || percent>100){
     printf("Only from 5 to 100\n");
-    return 0;
     }
     else{
     double price = get_double("Enter The Price: ");
@@ -16,8 +14,7 @@ double FreeInputDiscount(void){
     }
 }
 
-double ChoosableDiscount(void){
-    double staticpercent = get_double("Choose the discount: 5, 15, 20, 25, 50, 75, 85, 95; ");
+void ChoosableDiscount(double staticpercent){
     if(staticpercent != 5 && staticpercent != 15 && staticpercent != 20 && staticpercent != 25 && staticpercent != 50 && staticpercent != 75 && staticpercent != 85 && staticpercent != 95){
     printf("Please choose only previous percentages!\n");
     }
@@ -33,12 +30,12 @@ double ChoosableDiscount(void){
 int main(void){
     string answer = get_string("choose: FreeInputDiscount - ChoosableDiscount ; ");
     if(strcmp(answer, "FreeInputDiscount") == 0){
-    FreeInputDiscount();
-    }
-    else if(strcmp(answer, "ChoosableDiscount") == 0){
-    ChoosableDiscount();
-    }
-    else{
+        double staticpercent = get_double("Choose the discount: 5, 15, 20, 25, 50, 75, 85, 95; ");
+        FreeInputDiscount(staticpercent);
+    } else if(strcmp(answer, "ChoosableDiscount") == 0){
+        double percent = get_double("Percentage will be from 5 to 100; please enter your percentage: ");
+        ChoosableDiscount(percent);
+    } else{
     return 0;
     }
 }
